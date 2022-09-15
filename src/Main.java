0fcb20ed4f685a9fcc3f0f417b860c268416bf50
Andrew Sun <98356114+sunandrew03@users.noreply.github.com>
2022-09-15 01:56:44 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
 import java.util.Scanner;
 
+/**
+ * Runs this program.
+ */
 public class Main {
+  /**
+   * Main method to run this program.
+   * @param args
+   */
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.println("Type in the name of a valid agency or year to receive spending data for the" +
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Type in the name of a valid agency or year to receive spending data for the " +
             "specified input.");
+    while (true) {
+      String in = scan.nextLine();
+      Drawer d = new Drawer();
+      if (in.matches("\\d+")) {
+        try {
+          System.out.println(d.barGraph(Control.getYear(Integer.parseInt(in))));
+        } catch (Exception e) {
 
+        }
+      } else {
+        try {
+          System.out.println(d.lineGraph(Control.getAgency(in)));
+        } catch (Exception e) {
+
+        }
+      }
+      if (in.equalsIgnoreCase("q")) {
+        System.exit(1);
+      }
+    }
   }
 }
